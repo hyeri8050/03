a07de66f554d60dc2dcb7a647ffdf954cc48ff3e
Hyeri Park <phr66@naver.com>
2023-09-21 16:25:55 +0900

    week3-3

--- main.c
+++ main.c
@@ -4,17 +4,13 @@
 int main(int argc, char *argv[]) 
 {
     char c;
-    int i;
-    
-    printf("input a number:");
-    scanf("%c", &c);
-    
-    i = c - '0' ;
-    printf("The input number ASCII is %i\n", c);
-    printf("The input number is %i\n", i);
-    
-    system("PAUSE");
-    return 0;
 
+	printf("enter an character:");
+	scanf("%c", &c);
+
+	printf("The next character of %c (%d) is %c (%d)\n", c, c, c+1, c+1);
+
+	system("PAUSE");
+	return 0;
 
 }
